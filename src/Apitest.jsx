d5f3f3ae6a26b42d66e41e51a9bbed5438d7d38f
faisal795 <buntijani795@gmail.com>
2022-01-30 13:07:14 +0500

    modifide test project

--- src/Apitest.jsx
+++ src/Apitest.jsx
@@ -2,23 +2,18 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 function Apitest() {
-
-    const [Result, setResult] = useState([]);
+  const [Result, setResult] = useState([]);
 
   const fichapi = async () => {
     const data = await fetch(`https://api.atwrap.ae/products`);
     const result = await data.json();
-  
-    setResult(result);
-   
-  };
 
- 
+    setResult(result);
+  };
 
   useEffect(() => {
     fichapi();
   }, []);
-
 
   return (
     <>
@@ -29,38 +24,36 @@ function Apitest() {
           </div>
         </div>
         <div className="row">
-            {
-      Result.map((curntElmnt) => {
-        return (
-            <div className="col-md-4 p-y">
-            <div className="card">
-                <button>CROWD FAVORITE</button>
-              <img src={curntElmnt.image} alt="" />
-              <div className="card-body">
-                <div className="content">
-                  <div className="price">
-                    <h5>Saving Box</h5>
-                    <p>450 AED</p>
-                  </div>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Voluptatum fuga ad eos qui praesentium illum!
-                  </p>
-
-                  <div className="btn_div">
-                    <button>QUICK SHOP</button>
-                    <button>PERSONALIZE</button>
-                    <button>+</button>
+          {Result.map((curntElmnt) => {
+            return (
+              <div className="col-md-4 py-3" key={curntElmnt.id}>
+                <p className="title">
+                  {curntElmnt.tag ? curntElmnt.tag : "No Tag"}
+                </p>
+                <div className="card">
+                  <img src={curntElmnt.image} alt="" height={300} />
+                  <div className="card-body">
+                    <div className="content">
+                      <div className="price">
+                        <h5>{curntElmnt.name}</h5>
+                        <p>
+                          {curntElmnt.variants.map((price) => {
+                            return "Rs. " + price.price;
+                          })}
+                        </p>
+                      </div>
+                      <p>{curntElmnt.description.slice(0, 90)}</p>
+                      <div className="btn_div">
+                        <button>QUICK SHOP</button>
+                        <button>PERSONALIZE</button>
+                        <button>+</button>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-        )
-      })
-    }
-     
-         
+            );
+          })}
         </div>
       </div>
     </>
